b0534744639de0653cf2ca75e7d9424fe004e11c
KaeL <uberkael@gmail.com>
2026-02-01 02:19:08 +0100

    Update Locations

--- locations.py
+++ locations.py
@@ -5,37 +5,37 @@ LOCATIONS = {
     "chrome": {
         "windows": "%LOCALAPPDATA%\\Google\\Chrome\\User Data",
         "darwin": "$HOME/Library/Application Support/Google/Chrome",
-        "linux": "$HOME/.config/google-chrome/Default"
+        "linux": "$HOME/.config/google-chrome/Default",
     },
     "chromium": {
         "windows": "%LOCALAPPDATA%\\Chromium\\User Data",
         "darwin": "$HOME/Library/Application Support/Chromium",
-        "linux": "$HOME/.config/chromium/Default"
+        "linux": "$HOME/.config/chromium/Default",
     },
     "brave": {
         "windows": "%LOCALAPPDATA%\\BraveSoftware\\Brave-Browser\\User Data",
         "darwin": "$HOME/Library/Application Support/BraveSoftware/Brave-Browser",
-        "linux": "$HOME/.config/BraveSoftware/Brave-Browser/Default/"
+        "linux": "$HOME/.config/BraveSoftware/Brave-Browser/Default/",
     },
     "edge": {
         "windows": "%LOCALAPPDATA%\\Microsoft\\Edge\\User Data",
         "darwin": "$HOME/Library/Application Support/Microsoft Edge",
-        "linux": "$HOME/.config/microsoft-edge/Default"
+        "linux": "$HOME/.config/microsoft-edge/Default",
     },
     "vivaldi": {
         "windows": "%LOCALAPPDATA%\\Vivaldi\\User Data",
         "darwin": "$HOME/Library/Application Support/Vivaldi",
-        "linux": "$HOME/.config/vivaldi/Default"
+        "linux": "$HOME/.config/vivaldi/Default",
     },
     "opera": {
         "windows": "%LOCALAPPDATA%\\Opera Software\\Opera Stable",
         "darwin": "$HOME/Library/Application Support/com.operasoftware.Opera",
-        "linux": "$HOME/.config/opera"
+        "linux": "$HOME/.config/opera",
     },
     "helium": {
-        "windows": "%LOCALAPPDATA%\\net.imput.helium\\User Data",
+        "windows": "%LOCALAPPDATA%\\imput\\Helium\\User Data\\Default",
         "darwin": "$HOME/Library/Application Support/net.imput.helium",
-        "linux": "$HOME/.config/net.imput.helium/Default"
+        "linux": "$HOME/.config/net.imput.helium/Default",
     },
 }
 
